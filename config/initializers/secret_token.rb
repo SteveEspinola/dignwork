a3d320930f90673939f949d86130069b6ca38ccb
Steve Espinola <stephen.espinola@gmail.com>
2012-04-19 20:45:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dignwork::Application.config.secret_token = '9ac78833e1202f08e40463b5fe5f13d53655536b8eaf3545c6eb9679f762be54cd41a9f22b1b1f98b1cbfdbc4871654e5bc42cd3caf664948a6f3ab58aa01fac'
